40036ba6be43f61720c4163c996441cb117635f9
sandyatech <sandhya.mskgmail.com>
2023-03-14 19:53:43 +0000

    created simple form

---
+++ src/components/SpeciesName.tsx
@@ -0,0 +1,76 @@
+﻿interface SpeciesNameProps {
+	speciesName: string;
+	onChangeSpeciesName: (e: React.ChangeEvent<HTMLInputElement>) => void;
+
+	// You might be wondering what line 50 (e: React.ChangeEvent<HTMLInputElement>) is saying. 
+	//Let's break it down! 🔨😃 
+
+	// React has different ChangeEvents for different elements that might change. 
+	//Imagine a React.ChangeEvent < HTMLTextAreaElement > or React.ChangeEvent<HTMLSelectElement>, 
+	//these are the specific events you get back from changing specific types of element.
+	//This means when you write the code you know what type of element you're reacting to, so you might 
+	//want to do different things with a select versus an input text box.
+
+	// If you wanted the same function for all of them (for example, if we wanted to 
+	//refactor our forms so we had one function that handled all of them), you could write a 
+	//function with the signature
+	//(e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => void, 
+	//and pass that function as onChange to any of those kinds of elements!
+}
+
+const SpeciesName: React.FC<SpeciesNameProps> = ({ speciesName, onChangeSpeciesName }) => (
+    <div>
+        <label htmlFor='speciesName'>Species Name</label>
+		<input id='speciesName' type='text' value={speciesName} onChange={onChangeSpeciesName} />
+	</div>);
+
+export default SpeciesName;
+
+interface PlanetNameProps {
+	planetName: string;
+	onChangePlanetName: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const PlanetName: React.FC<PlanetNameProps> = ({ planetName, onChangePlanetName }) => (
+	<div>
+		<label htmlFor='planetName'>Planet Name</label>
+		<input id='planetName' type='text' value={planetName} onChange={onChangePlanetName} />
+	</div>);
+
+interface numberOfBeingsProps {
+	numberOfBeings: string;
+	onChangeNumberOfBeings: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const NumberOfBeings: React.FC<numberOfBeingsProps> = ({ numberOfBeings, onChangeNumberOfBeings}) => (
+	<div>
+		<label htmlFor='numberOfBeings'>Number Of Beings</label>
+		<input id='numberOfBeings' type='text' value={numberOfBeings} onChange={onChangeNumberOfBeings} />
+	</div>);
+
+interface add2Plus2Props {
+	add2Plus2: string;
+	onChangeAdd2Plus2: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+export const Add2Plus2: React.FC<add2Plus2Props> = ({ add2Plus2, onChangeAdd2Plus2 }) => (
+	<div>
+		<label htmlFor='add2Plus2'>What is 2 + 2</label>
+		<select id='add2Plus2' value={add2Plus2} onChange={onChangeAdd2Plus2}>
+			<option value="select">Select</option>
+			<option value="4">4</option>
+			<option value="Not4">Not 4</option>
+		</select>
+	</div>);
+
+interface reasonForSparingProps {
+	reasonForSparing: string;
+	onChangeReasonForSparing: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+export const ReasonForSparing: React.FC<reasonForSparingProps> = ({ reasonForSparing, onChangeReasonForSparing }) => (
+	<div>
+		<label htmlFor='reasonForSparing'>Reason For Sparing</label>
+		<textarea id='reasonForSparing' value={reasonForSparing} onChange={onChangeReasonForSparing}/>
+
+	</div>);
--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
+import { PlanetName, NumberOfBeings, Add2Plus2, ReasonForSparing } from './SpeciesName';
 import W12MHeader from './W12MHeader';
-
+import SpeciesName from './SpeciesName'
 const W12MForm = () => {
+	const [speciesName, setSpeciesName] = useState<string>('humans');
+	const [planetName, setPlanetName] = useState<string>('earth');
+	const [numberOfBeings, setNumberOfBeings] = useState<string>('10000');
+	const [add2Plus2, setAdd2Plus2] = useState<string>('select');
+	const [reasonForSparing, setreasonForSparing] = useState<string>('Please enter Area');
 	return (
 		<section className='w12MForm'>
 			<W12MHeader />
-			{/* REST OF FORM GOES HERE */}
+			<SpeciesName speciesName={speciesName} onChangeSpeciesName={(e: any) => setSpeciesName(e.target.value)} />
+			<PlanetName planetName={planetName} onChangePlanetName={(e: any) => setPlanetName(e.target.value)} />
+			<NumberOfBeings numberOfBeings={numberOfBeings} onChangeNumberOfBeings={(e: any) => setNumberOfBeings(e.target.value)} />
+			<Add2Plus2 add2Plus2={add2Plus2} onChangeAdd2Plus2={(e: any) => setAdd2Plus2(e.target.value)} />
+			<ReasonForSparing reasonForSparing={reasonForSparing} onChangeReasonForSparing={(e: any) => setreasonForSparing(e.target.value)} />
 		</section>
 	);
 };
